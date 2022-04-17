a07ab423731f72dea217accd4beea83330a76aec
GustavoBolanoss <99742985+GustavoBolanoss@users.noreply.github.com>
2022-04-17 20:15:02 -0300

    Add files via upload

---
+++ acquatec.sql
@@ -0,0 +1,12 @@
+create database acquatec;
+use acquatec;
+
+create table usuario(
+id int primary key auto_increment,
+nome varchar (45),
+telefone varchar(45),
+cpf varchar(45),
+email varchar(45),
+senha varchar(50)
+);
+
